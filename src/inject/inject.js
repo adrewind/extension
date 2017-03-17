49c21ae2f27780c47984f1c92b67384662529fad
xakdog <xakdog@gmail.com>
2017-03-18 01:49:34 +0300

    Basic algorithm for skipping given periods in YT player

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,47 @@
+
+
+
+class Player {
+
+    constructor() {
+        this.skip = [];
+        this.findVideoTag();
+        this.handleEvents();
+    }
+
+    findVideoTag() {
+        this.video = document.getElementsByTagName('video')[0];
+    }
+
+    seekTo(position) {
+        // console.log("SEEKING to " + position)
+        this.video.currentTime = position;
+    }
+
+    skipFragmet(from, to) {
+        this.skip.push([from, to]);
+    }
+
+    handleEvents() {
+        this.video.addEventListener("timeupdate", () => this.onTimeUpdate(), false);
+    }
+
+    onTimeUpdate() {
+        const current = this.video.currentTime;
+
+
+        for(let i=0; i < this.skip.length; ++i) {
+            const [start, end] = this.skip[i];
+
+            if(start > current || current > end)
+                continue;
+
+            return this.seekTo(end);
+        }
+    }
+}
+
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -7,6 +51,10 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
+
+        const conts = new AdditionalControls();
+        const player = new Player();
+        player.skipFragmet(30, 60);
 
 	}
 	}, 10);
