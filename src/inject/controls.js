dac9d3e0e3fc40d5e37dca21efcf9fc18f9285f0
xakdog <xakdog@gmail.com>
2017-03-27 01:55:05 +0300

    Fix player icon centering

--- src/inject/controls.js
+++ src/inject/controls.js
@@ -3,13 +3,13 @@ class AdditionalControls {
 
     constructor() {
         const svg = `
-            <svg id='Mark_AD' data-name='Mark AD' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 36 36'>
+            <svg id="Mark_AD" data-name="Mark AD" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
                 <defs><style>.mrkad-1{fill:#fff;}.mrkad-2{fill:none;stroke:#f12b24;stroke-miterlimit:10;stroke-width:2px;}</style></defs>
-                <title>32cursor-ad</title>
-                <path class='mrkad-1' d='M18.07,23.29H15.2L14.74,22h-3l-.46,1.31H8.43L11.85,14h2.81ZM13.62,18.4a12.31,12.31,0,0,1-.35-1.4c-.16.64-.21.85-.39,1.42,0,0-.2.64-.51,1.57h1.74Z'/>
-                <path class='mrkad-1' d='M22.9,14a4.3,4.3,0,0,1,4.67,4.6c0,3.08-1.69,4.66-4.72,4.66H18.76V14Zm-1.35,6.92h1.12c1.39,0,2.08-.87,2.08-2.26a2.11,2.11,0,0,0-2.35-2.33h-.85Z'/>
-                <line class='mrkad-2' x1='6.81' y1='12.69' x2='6.81' y2='25.38'/><line class='mrkad-2' x1='10.04' y1='12.69' x2='3.58' y2='12.69'/>
-                <line class='mrkad-2' x1='10.04' y1='25.38' x2='3.58' y2='25.38'/>
+                <path class="mrkad-1" d="M20.5,22.4H17.62l-.46-1.31h-3l-.46,1.31H10.86l3.42-9.26h2.81ZM16,17.51a12.31,12.31,0,0,1-.35-1.4c-.16.64-.21.85-.39,1.42,0,0-.2.64-.51,1.57h1.74Z"/>
+                <path class="mrkad-1" d="M25.32,13.14A4.3,4.3,0,0,1,30,17.74c0,3.08-1.69,4.66-4.72,4.66H21.19V13.14ZM24,20.06h1.12c1.39,0,2.08-.87,2.08-2.26a2.11,2.11,0,0,0-2.35-2.33H24Z"/>
+                <line class="mrkad-2" x1="9.24" y1="11.79" x2="9.24" y2="24.49"/>
+                <line class="mrkad-2" x1="12.46" y1="11.79" x2="6.01" y2="11.79"/>
+                <line class="mrkad-2" x1="12.46" y1="24.49" x2="6.01" y2="24.49"/>
             </svg>`;
 
 
