0cbc869766fb8df06c7899d37c231f8f9165f446
xakdog <xakdog@gmail.com>
2017-04-20 02:25:26 +0300

    Use mocky.io for tests

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 
-// TODO: Find a way to create single configuration file
-export const API_ENDPOINT_VIDEOS = 'https://gv.adrewind.net';
-export const API_ENDPOINT_REPORTS = 'https://tk.adrewind.net';
 export const ELEMENT_LOCATE_FREQ = 500; // ms
-
-// TODO: use http://www.mocky.io/v2/58f7e7b21100001a02a172db endpoint for tests
+export const API_ENDPOINT_VIDEOS = environment === 'production' ?
+    'https://gv.adrewind.net' : 'http://www.mocky.io/v2/58f7e7b21100001a02a172db';
+export const API_ENDPOINT_REPORTS = environment === 'production' ?
+    'https://tk.adrewind.net' : 'http://www.mocky.io/v2/58f7e7b21100001a02a172db';
 
 export const SYNC_ALARM_PERIOD = 3; // minutes
 export const SYNC_MATURITY_TRESHOLD = 5; // minutes. Items must be untouched 5 minutes or more
--- test/base.js
+++ test/base.js
@@ -5,4 +5,5 @@ const FakeChromeStorage = require('./base/fake-chrome-storage');
 global.chrome = {
     storage: { local: new FakeChromeStorage() },
 };
+global.environment = 'development';
 global.XMLHttpRequest = fakeXHR.xhr;
